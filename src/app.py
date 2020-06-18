cc912716cc85728c21241ed7a1398017488945e9
LeCuay <suso.becerra98@gmail.com>
2020-06-18 22:03:58 +0200

    Started project for dispatching

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import logging
+
+from flask import Flask, json, request
+
+LOGGER = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+
+@app.route('/github/', methods=['POST'])
+def github_payload():
+    if request.headers['Content-Type'] == 'application/json':
+        payload = request.json
+        LOGGER.info('Payload received: %s', payload)
+        return json.dumps(request.json)
+
+
+if __name__ == '__main__':
+    app.run()
